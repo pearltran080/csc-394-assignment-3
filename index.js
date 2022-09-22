f82e3bd57501b6211356e9105e74c313a4ea284d
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2022-09-22 16:17:28 -0500

    pug

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+// view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+
+// database
+const Pool = require('pg').Pool
+
+var connection = null;
+if (process.env.DATABASE_URL != null) {
+    connection = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    }
+} else {
+    connection = {
+        user: 'postgres',
+        host: '18.218.121.53',
+        database: 'postgres',
+        password: 'galaxy88',
+        port: 5432
+    }
+}
+
+const pool = new Pool(connection)
+
 app.get ('/', function(req, res) {
-    res.send('test')
+    // res.render('index', {
+    //     title: "Pearleen Tran's HW3"
+    // })
+
+    pool.query('SELECT VERSION()', (err, version_results) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+
+        res.render('index', {
+            title: "Pearleen Tran's HW3",
+            databaseVer: version_results.rows[0].version
+        })
+    })
 })
 
 app.listen(3000, () => {
