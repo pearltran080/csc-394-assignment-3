87a511bea483ddfc37b1ab5dec4e032e2e7a5540
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2022-09-22 21:52:12 -0500

    Revert "Update index.js"
    This reverts commit 5850b8bca5927c466c26b0b3eec3c93a45476b5e.

--- index.js
+++ index.js
@@ -7,33 +7,31 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // database
-const Pool = require('pg').Pool
-var connection = null;
-if (process.env.DATABASE_URL != null) {
-    connection = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-    }
-} else {
-    connection = {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'galaxy88',
-        port: 5432
-    }
-}
-const pool = new Pool(connection)
+const {Client} = require('pg')
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'galaxy88',
+    port: 5432
+})
+
+client.connect();
+
 app.get ('/', function(req, res) {
-    pool.query('SELECT VERSION()', (err, version_results) => {
+    client.query('SELECT VERSION()', (err, version_results) => {
         if (err) {
-          return console.error('Error executing query', err.stack)
+            client.end()
+            return console.error('Error executing query', err.stack)
         }
+
         res.render('index', {
             title: "Pearleen Tran's HW3",
-            databaseVer: version_results.rows[0].version
+            databaseVer: version_results.rows
         })
     })
+    client.end()
 })
 
 app.listen(3000, () => {
