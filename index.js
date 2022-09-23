5850b8bca5927c466c26b0b3eec3c93a45476b5e
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2022-09-22 21:50:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,33 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // database
-const {Client} = require('pg')
-
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'galaxy88',
-    port: 5432
-})
-
-client.connect();
-
+const Pool = require('pg').Pool
+var connection = null;
+if (process.env.DATABASE_URL != null) {
+    connection = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    }
+} else {
+    connection = {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'galaxy88',
+        port: 5432
+    }
+}
+const pool = new Pool(connection)
 app.get ('/', function(req, res) {
-    client.query('SELECT VERSION()', (err, version_results) => {
+    pool.query('SELECT VERSION()', (err, version_results) => {
         if (err) {
-            client.end()
-            return console.error('Error executing query', err.stack)
+          return console.error('Error executing query', err.stack)
         }
-
         res.render('index', {
             title: "Pearleen Tran's HW3",
-            databaseVer: version_results.rows
+            databaseVer: version_results.rows[0].version
         })
     })
-    client.end()
 })
 
 app.listen(3000, () => {
