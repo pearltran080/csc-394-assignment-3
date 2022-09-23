535e1cb30a9e9ff4c6bdc2ef8064b7974ce8d13e
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2022-09-23 17:47:55 -0500

    pg promise

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:galaxy88@db-hw3.cfssmhuslkyb.us-east-2.rds.amazonaws.com:5432/postgres')
 
 // view engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // database
-const {Client} = require('pg')
-
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'galaxy88',
-    port: 5432
-})
-
-client.connect();
-
-app.get ('/', function(req, res) {
-    client.query('SELECT VERSION()', (err, version_results) => {
-        if (err) {
-            client.end()
-            return console.error('Error executing query', err.stack)
-        }
-
-        res.render('index', {
-            title: "Pearleen Tran's HW3",
-            databaseVer: version_results.rows
+app.get('/', function(req, res) {
+    db.one('SELECT VERSION()')
+        .then((data) => {
+            res.render('index', {
+                title: "Pearleen Tran's HW3",
+                databaseVer: data.version
+            })
         })
-    })
-    client.end()
+        .catch((error) => {
+            console.log('ERROR:', error)
+          })
 })
 
 app.listen(3000, () => {
